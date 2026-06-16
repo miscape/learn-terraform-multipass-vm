64ad341b56b65f610f55565e77496143bc30afb2
Mirko Scapellato <scapellato.mirko@gmail.com>
2026-06-16 10:09:12 +0200

    Move lab values into terraform tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,21 @@
+vm_image = "24.04"
+
+vm_instances = {
+  "vm-01" = {
+    cpus   = 1
+    memory = "1G"
+    disk   = "5G"
+  }
+
+  "vm-02" = {
+    cpus   = 1
+    memory = "1G"
+    disk   = "5G"
+  }
+
+  "vm-03" = {
+    cpus   = 1
+    memory = "1G"
+    disk   = "5G"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "vm_image" {
   description = "Immagine Ubuntu da usare per le VM Multipass"
   type        = string
-  default     = "24.04"
 }
 
 variable "vm_instances" {
@@ -11,24 +10,4 @@ variable "vm_instances" {
     memory = string
     disk   = string
   }))
-
-  default = {
-    "vm-01" = {
-      cpus   = 1
-      memory = "1G"
-      disk   = "5G"
-    }
-
-    "vm-02" = {
-      cpus   = 1
-      memory = "1G"
-      disk   = "5G"
-    }
-
-    "vm-03" = {
-      cpus   = 1
-      memory = "1G"
-      disk   = "5G"
-    }
-  }
 }
